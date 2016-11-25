10417e4a43269401e7de8be224825da3396b2641
spacemh <spaceyspacespace@gmail.com>
2016-11-25 14:58:14 +0000

    fix analytics and infintate scroll

--- public/js/extras.min.js
+++ public/js/extras.min.js
@@ -3,7 +3,7 @@
 
 // templates are imported via gulp job
 angular.module('templates', []);
-angular.module('dnsim', ['ngRoute','templates']);
+angular.module('dnsim', ['ngRoute','angulartics','angulartics.google.analytics','templates', 'ngAria', 'infinite-scroll']);
 
 var baseElement = angular.element(document).find('base');
 
