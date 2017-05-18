2f09b22302bea3914041783dc234d16e7dfd9f4b
spacemh <spaceyspacespace@gmail.com>
2017-05-18 13:16:18 +0000

    fix angular 1.6 error

--- ui/ng-app.js
+++ ui/ng-app.js
@@ -8,11 +8,12 @@ var baseElement = angular.element(document).find('base');
 if(baseElement.length) {
     angular.module('dnsim', ['ngRoute','angulartics','angulartics.google.analytics','templates', 'ngAria', 'infinite-scroll']);
     angular.module('dnsim').config(['$locationProvider', setupHtml5Mode]);
+    angular.module('dnsim').config(['$compileProvider', allowAutoBindings]);
 }
 else {
     angular.module('dnsim', ['ngRoute','templates']);
 }
-angular.module('dnsim').config(['$compileProvider', allowAutoBindings]);
+
 function allowAutoBindings($compileProvider) {
     // angular 1.6 requires use of $onInit unless this config is set
     // https://toddmotto.com/angular-1-6-is-here
